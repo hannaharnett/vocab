e5c2689490ce25f6673998f6af88e65196526e04
Hannah Arnett <hannahtesaker@gmail.com>
2020-09-02 10:50:20 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>VOCAB</h1>
+      </header>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
